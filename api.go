7c271ad2d6e96d18ec9a082afc4e4222f00e56ee
Jingwen Owen Ou <jingweno@gmail.com>
2014-02-05 14:31:05 -0800

    Remove API

--- api.go
+++ api.go
@@ -2,62 +2,15 @@ package main
 
 import (
 	"net/http"
-	"regexp"
 	"strconv"
-
-	"github.com/codegangsta/martini"
 )
 
 func NewApi(sphero Sphero) Api {
-	return &ApiMartini{sphero}
+	return nil
 }
 
 type Api interface {
 	Handler() http.Handler
-}
-
-type ApiPlain struct {
-	S Sphero
-}
-
-func (a *ApiPlain) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	rgbRegexp := regexp.MustCompile(`/rgb/(\d+),(\d+),(\d+)`)
-	path := req.URL.Path
-	if req.Method == "PUT" && rgbRegexp.MatchString(path) {
-		match := rgbRegexp.FindStringSubmatch(path)
-		setRGB(a.S, match[1], match[2], match[3])
-
-		resp.WriteHeader(201)
-	} else {
-		http.NotFound(resp, req)
-	}
-}
-
-func (a *ApiPlain) Handler() http.Handler {
-	return a
-}
-
-type ApiMartini struct {
-	S Sphero
-}
-
-func (a *ApiMartini) Handler() http.Handler {
-	m := martini.Classic()
-	m.Put("/rgb/:rgb", func(params martini.Params) (int, string) {
-		rgb := params["rgb"]
-
-		rgbRegexp := regexp.MustCompile(`^(\d+),(\d+),(\d+)$`)
-		if !rgbRegexp.MatchString(params["rgb"]) {
-			return 400, "Invalid format of rgb"
-		}
-
-		match := rgbRegexp.FindStringSubmatch(rgb)
-		setRGB(a.S, match[1], match[2], match[3])
-
-		return 201, "ok"
-	})
-
-	return m
 }
 
 func setRGB(s Sphero, r, g, b string) {
